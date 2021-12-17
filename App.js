c58e6bd1c6448c80f8d5ef2014d098e4c96feb5e
victor bobkov <victorbobkov@gmail.com>
2021-12-18 00:41:21 +0300

    base app

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react'
+import {
+   StyleSheet,
+   Button,
+   Text,
+   TextInput,
+   View,
+   ScrollView,
+   FlatList
+} from 'react-native'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+   const [enteredTask, setEnteredTask] = useState('')
+   const [tasks, setTasks] = useState([])
+
+   const goalInputHandler = (enteredText) => {
+      setEnteredTask(enteredText)
+   }
+   
+   const addGoalHandler = () => {
+      setTasks(currentTasks => [...currentTasks, {key: Math.random().toString(), value: enteredTask}])
+   }
+
+   return (
+      <View style={styles.screen}>
+         <View style={styles.inputContainer}>
+            <TextInput
+               placeholder="Добавить задачу"
+               style={styles.input}
+               onChangeText={goalInputHandler}
+               value={enteredTask}
+            />
+            <Button title="ADD" onPress={addGoalHandler} />
+         </View>
+         <FlatList
+            data={tasks}
+            renderItem={itemData => (
+               <View style={styles.listItem}>
+                  <Text>{itemData.item.value}</Text>
+               </View>
+            )}
+         />
+      </View>
+   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+   screen: {
+      padding: 50
+   },
+   inputContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center'
+   },
+   input: {
+      width: '80%',
+      borderColor: 'black',
+      borderWidth: 1,
+      padding: 10
+   },
+   listItem: {
+      padding: 10,
+      marginVertical: 10,
+      backgroundColor: '#ccc',
+      borderColor: 'black',
+      borderWidth: 1
+   }
+})
